973c8a2ec86011221ba6dd3d08d2464ddec9082f
Symphoney <rijarahmed1@gmail.com>
2025-12-18 22:48:48 -0600

    Module Implementation

--- Employee.cpp
+++ Employee.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "Employee.h"
+module Employee;
+import <iostream>;
+import <format>;
+
+namespace Records {
+    Employee::Employee(const std::string& firstName, const std::string& lastName)
+        : m_firstName{firstName}, m_lastName{lastName} {
+    }
+
+    void Employee::promote(int raiseAmount) {
+        setSalary(getSalary() + raiseAmount);
+    }
+
+    void Employee::demote(int demeritAmount) {
+        setSalary(getSalary() - demeritAmount);
+    }
+
+    void Employee::hire() {m_hired = true;}
+    void Employee::fire() {m_hired = false;}
+
+    void Employee::display() const{
+        std::cout << std::format("Employee: {}, {}", getLastName(), getFirstName()) << '\n';
+        std::cout << "-----------------------" << '\n';
+        std::cout << (isHired() ? "Current Employee" : "Former Employee") << '\n';
+        std::cout << std::format("Employee Number: {}", getEmployeeNumber()) <<  '\n';
+        std::cout << std::format("Salary: ${}", getSalary()) << '\n';
+        std::cout << '\n';
+    }
+
+    void Employee::setFirstName(const std::string& firstName) {
+        m_firstName = firstName;
+    }
+
+    const std::string& Employee::getFirstName() const {
+        return m_first_Name;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+import Employee;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Testing Employee class." << '\n';
+    Record::Employee emp{"John", "Doe"};
+    emp.setFirstName("Sarah");
+    emp.setLastName("Smith");
+    emp.setEmployeeNumber(12345);
+    emp.setSalary(50'000);
+    emp.promote();
+    emp.promote(50);
+    emp.hire();
+    emp.display();
     return 0;
 }
